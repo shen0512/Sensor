7fdf3eb5c611a430aab232fd974cd15226fdf963
shen0512 <a581030w@gmail.com>
2017-03-01 22:27:33 +0800

    update0301

--- app/src/main/java/com/example/user/sensor/MainActivity.java
+++ app/src/main/java/com/example/user/sensor/MainActivity.java
@@ -9,71 +9,64 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    SensorManager sensorMgrAccelerometer,sensorMgrLight;
-    TextView showAccelerometer,showLight;
+    SensorManager sensorMgr;
+    TextView showAccelerometer,showGravity,showRotationV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorMgrAccelerometer = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorMgrLight = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorMgr= (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        showAccelerometer = (TextView) findViewById(R.id.accelerometer);
-        showLight = (TextView) findViewById(R.id.light);
+        showAccelerometer = (TextView) findViewById(R.id.acceleromter);
+        showGravity = (TextView) findViewById(R.id.gravity);
+        showRotationV=(TextView) findViewById(R.id.rotationV);
     }
 
-    SensorEventListener listenerACCCELEROMETER=new SensorEventListener() {
+    SensorEventListener listener=new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
             Sensor sensor=event.sensor;
-            StringBuilder sensorInfo=new StringBuilder();
-            sensorInfo.append("Sensor Name "+sensor.getName()+"\n");
-
-            sensorInfo.append("Value: \n");
-            float[] values=event.values;
-            sensorInfo.append("values[0] "+values[0]+"\n");
-            sensorInfo.append("values[1] "+values[1]+"\n");
-            sensorInfo.append("values[2] "+values[2]+"\n");
-
-            showAccelerometer.setText(sensorInfo);
+            switch(sensor.getType()){
+                case Sensor.TYPE_ACCELEROMETER:
+                    showAccelerometer.setText("Sensor name: "+sensor.getName()+"\n");
+                    showAccelerometer.append("Values[0]: "+event.values[0]+"\n");
+                    showAccelerometer.append("Values[1]: "+event.values[1]+"\n");
+                    showAccelerometer.append("Values[2]: "+event.values[2]+"\n");
+                    break;
+                case Sensor.TYPE_GRAVITY:
+                    showGravity.setText("Sensor name: "+sensor.getName()+"\n");
+                    showGravity.append("Values[0]: "+event.values[0]+"\n");
+                    showGravity.append("Values[1]: "+event.values[1]+"\n");
+                    showGravity.append("Values[2]: "+event.values[2]+"\n");
+                    break;
+                case Sensor.TYPE_ROTATION_VECTOR:
+                    showRotationV.setText("Sensor name: "+sensor.getName()+"\n");
+                    showRotationV.append("Values[0]: "+event.values[0]+"\n");
+                    showRotationV.append("Values[2]: "+event.values[1]+"\n");
+                    showRotationV.append("Values[2]: "+event.values[1]+"\n");
+                    break;
+            }
         }
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {}
     };
 
-    SensorEventListener listenerLIGHT=new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            Sensor sensor=event.sensor;
-            StringBuilder sensorInfo=new StringBuilder();
-            sensorInfo.append("Sensor Name "+sensor.getName()+"\n");
-            sensorInfo.append("values "+event.values[0]+"\n");
-
-            showLight.setText(sensorInfo);
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
     @Override
     protected void onResume(){
         super.onResume();
-        sensorMgrAccelerometer.registerListener(listenerACCCELEROMETER, sensorMgrAccelerometer.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+        sensorMgr.registerListener(listener, sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_NORMAL);
-        sensorMgrAccelerometer.registerListener(listenerLIGHT,sensorMgrLight.getDefaultSensor(Sensor.TYPE_LIGHT),
+        sensorMgr.registerListener(listener,sensorMgr.getDefaultSensor(Sensor.TYPE_GRAVITY),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorMgr.registerListener(listener,sensorMgr.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
                 SensorManager.SENSOR_DELAY_NORMAL);
 
     }
     @Override
     protected void onPause(){
         super.onPause();
-        sensorMgrAccelerometer.unregisterListener(listenerACCCELEROMETER);
-        sensorMgrLight.unregisterListener(listenerLIGHT);
-
+        sensorMgr.unregisterListener(listener);
     }
 }
